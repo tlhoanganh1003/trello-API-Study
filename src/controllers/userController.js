6a1e30f02824859284c02597b69ed7fcdff5299c
tlhoanganh1003 <tlhoanganh1003@gmail.com>
2024-06-06 19:52:04 +0700

    lesson_7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,6 @@
 import { StatusCodes } from 'http-status-codes'
 import { userService } from '~/services/userService'
 
-
 const createNew = async (req, res, next) => {
   try {
     const createdUser = await userService.createNew(req.body)
@@ -13,8 +12,26 @@ const createNew = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
-
-export const userController = {
-  createNew
+const verifyAccount = async (req, res, next) => {
+  try {
+    const result = await userService.verifyAccount(req.body)
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) { next(error) }
 }
 
+const login = async (req, res, next) => {
+  try {
+    const result = await userService.login(req.body)
+
+    // xử lý trả về http only cookie cho phía trình duyệt
+    console.log('login: ', result)
+
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) { next(error) }
+}
+
+export const userController = {
+  createNew,
+  verifyAccount,
+  login
+}
--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -6,13 +6,15 @@ import express from 'express'
 import { userValidation } from '~/validations/userValidation'
 import { userController } from '~/controllers/userController'
 
-
 const Router = express.Router()
-
 
 Router.route('/register')
   .post(userValidation.createNew, userController.createNew)
 
+Router.route('/verify')
+  .put(userValidation.verifyAccount, userController.verifyAccount)
+
+Router.route('/login')
+  .post(userValidation.login, userController.login)
 
 export const userRoute = Router
-
