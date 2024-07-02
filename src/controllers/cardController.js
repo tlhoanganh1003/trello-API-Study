55a74566ac728fea69d1ecf1a6fd0dfa0fe51bcc
tlhoanganh1003 <tlhoanganh1003@gmail.com>
2024-07-02 15:03:41 +0700

    lesson_14

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -13,6 +13,18 @@ const createNew = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const update = async (req, res, next) => {
+  try {
+    const cardId = req.params.id
+    const updatedCard = await cardService.update(cardId, req.body)
+
+
+    res.status(StatusCodes.OK).json(updatedCard)
+  } catch (error) { next(error) }
+}
+
+
 export const cardController = {
-  createNew
+  createNew,
+  update
 }
