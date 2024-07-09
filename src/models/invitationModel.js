ff6fc4a6b21646627d6080927f0a4fefe3c1495c
tlhoanganh1003 <tlhoanganh1003@gmail.com>
2024-07-09 21:36:31 +0700

    lesson_17

--- src/models/invitationModel.js
+++ src/models/invitationModel.js
@@ -8,7 +8,8 @@ import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { INVITATION_TYPES, BOARD_INVITATION_STATUS } from '~/utils/constants'
-
+import { userModel } from './userModel'
+import { boardModel } from './boardModel'
 
 // Define Collection (name & schema)
 const INVITATION_COLLECTION_NAME = 'invitations'
@@ -99,6 +100,42 @@ const update = async (invitationId, updateData) => {
     return result
   } catch (error) { throw new Error(error) }
 }
+// Query tổng hợp (aggregate) để lấy những bản ghi intivation thuộc về một thằng user cụ thể.
+const findByUser = async (userId) => {
+  try {
+    const queryConditions = [
+      { inviteeId: new ObjectId(userId) },
+      { _destroy: false }
+    ]
+
+    const results = await GET_DB().collection(INVITATION_COLLECTION_NAME).aggregate([
+      { $match: { $and: queryConditions } },
+      { $lookup: {
+        from: userModel.USER_COLLECTION_NAME,
+        localField: 'inviterId', // người đi mời
+        foreignField: '_id',
+        as: 'inviter',
+        pipeline: [{ $project: { 'password': 0, 'verifyToken': 0 } }]
+      } },
+      { $lookup: {
+        from: userModel.USER_COLLECTION_NAME,
+        localField: 'inviteeId', // người đi mời
+        foreignField: '_id',
+        as: 'invitee',
+        pipeline: [{ $project: { 'password': 0, 'verifyToken': 0 } }]
+      } },
+      { $lookup: {
+        from: boardModel.BOARD_COLLECTION_NAME,
+        localField: 'boardInvitation.boardId',
+        foreignField: '_id',
+        as: 'board'
+      } }
+
+    ]).toArray()
+
+    return results
+  } catch (error) { throw new Error(error) }
+}
 
 
 export const invitationModel = {
@@ -106,6 +143,7 @@ export const invitationModel = {
   INVITATION_COLLECTION_SCHEMA,
   createNewBoardInvitation,
   findOneById,
-  update
+  update,
+  findByUser
 }
 
